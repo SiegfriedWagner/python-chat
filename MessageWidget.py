6b8d3772af89552ac67cbde320c9e890983009d1
SiegfriedWagner <mateus.chojnowski@gmail.com>
2020-02-22 15:16:24 +0100

    minor cleanup and ui prettyfied

--- MessageWidget.py
+++ MessageWidget.py
@@ -5,25 +5,12 @@ import os
 from message import ChatMessage
 
 
-class MessageWidget(QFrame):
+class MessageWidget(QWidget):
 
     def __init__(self, message: ChatMessage, font_size, font_family, flags=None, *args, **kwargs):
         super().__init__(flags, *args, **kwargs)
-        widget = QWidget(None)
-        loadUi(os.path.join(os.path.dirname(__file__), "message.ui"), widget)
-        widget.messageLabel.setFont(QFont(font_family, font_size))
-        widget.senderLabel.setFont(QFont(font_family, font_size))
-        layout = QVBoxLayout()
-        layout.setSpacing(0)
-        layout.setContentsMargins(10, 0, 10, 0)
-        layout.addWidget(widget)
-        widget.messageLabel: QLabel = widget.messageLabel
-        widget.senderLabel: QLabel = widget.senderLabel
-        widget.setAutoFillBackground(True)
-        # self.senderAndMessengerSpacer: QSpacerItem = self.senderAndMessengerSpacer
-        widget.messageLabel.setText(message.text)
-        widget.senderLabel.setText(message.sender)
-        self.setLineWidth(10)
-        self.setFrameStyle(QFrame.StyledPanel + QFrame.Raised)
-        self.setLayout(layout)
-
+        loadUi(os.path.join(os.path.dirname(__file__), "message.ui"), self)
+        self.messageLabel.setFont(QFont(font_family, font_size))
+        self.senderLabel.setFont(QFont(font_family, font_size))
+        self.messageLabel.setText(message.text)
+        self.senderLabel.setText(message.sender)
